daba08042fba7e25d0ddffb4336995553cc09ca1
Falk Hoppe <falkhoppe81@gmail.com>
2019-04-27 08:51:05 +0200

    add css manifest file loading

--- index.js
+++ index.js
@@ -6,14 +6,29 @@ const { landingPage } = require('./lib/controller.js')
 
 app.use('/styles', express.static('dist/styles'))
 
-app.get('/', landingPage)
+const prepareApp = async () => {
+  const assetManifest = await require('./lib/asset-manifest.js')()
 
-const port = process.env.PORT
+  const viewOptions = { assetManifest }
 
-if (!port) {
-  throw new Error('PORT required')
+  app.get('/', landingPage({ viewOptions }))
+
+  return app
 }
 
-app.listen(port, () => {
-  console.log(`app listening to PORT: ${port}`)
-})
+prepareApp()
+  .then(app => {
+    const port = process.env.PORT
+
+    if (!port) {
+      throw new Error('PORT required')
+    }
+
+    app.listen(port, () => {
+      console.log(`app listening to PORT: ${port}`)
+    })
+  })
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
