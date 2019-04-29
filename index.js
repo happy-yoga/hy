b5e0eaeb0f588453eb06ede1bc755d8c8ec5dbc0
Falk Hoppe <falkhoppe81@gmail.com>
2019-04-29 10:03:23 +0200

    add favicons and images

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = require('./lib/app.js')
 const { landingPage } = require('./lib/controller.js')
 
 app.use('/styles', express.static('dist/styles'))
+app.use('/images', express.static('dist/images'))
+app.use('/', express.static('favicon'))
 
 const prepareApp = async () => {
   const assetManifest = await require('./lib/asset-manifest.js')()
