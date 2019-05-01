e7ac6c084765bfad93de93bdf60c04ba66655a14
Falk Hoppe <falkhoppe81@gmail.com>
2019-05-01 11:01:21 +0200

    hidden message and parallax logo

--- build/js-vendor-build.sh
+++ build/js-vendor-build.sh
@@ -2,3 +2,4 @@
 
 mkdir -p ./tmp/scripts/vendor/.
 cp ./node_modules/document-register-element/build/document-register-element.js tmp/scripts/vendor/.
+cp ./scripts/vendor/rellax-patched.js tmp/scripts/vendor/.
