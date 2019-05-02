bf244bba132d5360813e45edfd7fa3de86611cad
Falk Hoppe <falkhoppe81@gmail.com>
2019-05-02 09:05:04 +0200

    improve header opacity slide

--- scripts/vendor/hearts-bg.js
+++ scripts/vendor/hearts-bg.js
@@ -3,21 +3,15 @@ Based on the work of https://codepen.io/lovefield
 
 see https://codepen.io/lovefield/pen/vEvqZV
 */
-setInterval(function () {
-  var rNum = Math.floor(Math.random() * 40) + 1
-  var rSize = Math.floor(Math.random() * 15) + 10
-  var rLeft = Math.floor(Math.random() * 100) + 1
-  var rTime = Math.floor(Math.random() * 5) + 5
 
-  var bgHeart = document.querySelector('.bg-heart')
-  bgHeart.innerHTML =
-    'happy body. happy life.' +
+function heart (rSize, rLeft, rTime, rNum) {
+  return (
     "<div class='heart' style='width:" +
     rSize +
     'px;height:' +
     rSize +
     'px;left:' +
-    rLeft +
+    (rLeft + rNum) +
     '%;background:rgba(255, 255, 255);-webkit-animation:love ' +
     rTime +
     's ease;-moz-animation:love ' +
@@ -26,29 +20,47 @@ setInterval(function () {
     rTime +
     's ease;animation:love ' +
     rTime +
-    "s ease'></div>" +
-    "<div class='heart' style='width:" +
-    (rSize - 10) +
-    'px;height:' +
-    (rSize - 10) +
-    'px;left:' +
-    (rLeft + rNum) +
-    '%;background:rgba(255,255,255);-webkit-animation:love ' +
-    (rTime + 5) +
-    's ease;-moz-animation:love ' +
-    (rTime + 5) +
-    's ease;-ms-animation:love ' +
-    (rTime + 5) +
-    's ease;animation:love ' +
-    (rTime + 5) +
     "s ease'></div>"
+  )
+}
+
+setInterval(function () {
+  var rNum = Math.floor(Math.random() * 40) + 1
+  var rSize = Math.floor(Math.random() * 15) + 10
+  var rLeft = Math.floor(Math.random() * 100) + 1
+  var rTime = Math.floor(Math.random() * 15) + 5
+
+  var bgHeart = document.querySelector('.bg-heart')
+  bgHeart.innerHTML =
+    'happy body. happy life.' +
+    heart(rSize, rLeft, rTime, 0) +
+    heart(rSize - 10, rLeft, rTime + 5, rNum) +
+    heart(
+      rSize - 20,
+      rLeft + Math.floor(Math.random() * 40) + 1,
+      rTime + 7,
+      rNum
+    ) +
+    heart(
+      rSize - 10,
+      rLeft - Math.floor(Math.random() * 40) + 1,
+      rTime + 7,
+      rNum
+    ) +
+    heart(
+      rSize - 20,
+      rLeft + Math.floor(Math.random() * 40) + 1,
+      rTime - 2,
+      rNum
+    )
+
   var hearts = document.querySelectorAll('.heart')
 
   hearts.forEach(function (heart) {
     var top = heart.style.top.replace(/[^-\d\.]/g, '')
     // var width = heart.style.width.replace(/[^-\d\.]/g, '')
-    if (top <= -100) {
+    if (top <= -300) {
       heart.remove()
     }
   })
-}, 500)
+}, 1500)
